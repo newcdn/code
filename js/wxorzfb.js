4d834ffd05253a1c01f635289abd62eda583169f
ming <59531725+newcdn@users.noreply.github.com>
2021-05-11 09:46:56 +0800

    Update wxorzfb.js

--- js/wxorzfb.js
+++ js/wxorzfb.js
@@ -1,11 +1,19 @@
     // 对浏览器的UserAgent进行正则匹配，不含有微信独有标识的则为其他浏览器
     var useragent = navigator.userAgent;
-    if (useragent.match(/MicroMessenger/i) != 'MicroMessenger');
-    if (useragent.match(/Alipay/i) != 'Alipay') {
+    if (useragent.match(/MicroMessenger/i) != 'MicroMessenger' && useragent.match(/Alipay/i) != 'Alipay' && useragent.match(/CloudPay/i) != 'CloudPay') {
         // 这里警告框会阻塞当前页面继续加载
-        alert('已禁止本次访问：您必须使用微信或支付宝扫码！');
+window.alert = function (name) {
+  const iframe = document.createElement('IFRAME');
+  iframe.style.display = 'none';
+  iframe.setAttribute('src', 'data:text/plain,');
+  document.documentElement.appendChild(iframe);
+  window.frames[0].window.alert(name);
+  iframe.parentNode.removeChild(iframe);
+};
+        alert('已禁止本次访问：您必须使用微信内置浏览器访问本页面！');
         // 以下代码是用javascript强行关闭当前页面
         var opened = window.open('about:blank', '_self');
         opened.opener = null;
         opened.close();
-    } 
+    }
+
