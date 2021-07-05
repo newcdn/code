62ff7b1a8134ae11e112feb7bf94e37f658b64d6
ming <59531725+newcdn@users.noreply.github.com>
2021-07-05 23:49:03 +0800

    Update wxtzbrowser.js

--- js/wxtzbrowser.js
+++ js/wxtzbrowser.js
@@ -1,16 +1,18 @@
-    $(window).on("load",function(){
-        var winHeight = $(window).height();
-        function is_weixin() {
+        var is_weixin = (function () {
             var ua = navigator.userAgent.toLowerCase();
-            if (ua.match(/MicroMessenger/i) == "micromessenger") {
+            if (ua.match(/MicroMessenger/i) == "micromessenger") {//微信中打开网页
+                return true;
+            } else if (ua.match(/QQ/i) == "qq") {//qq里打开网页
                 return true;
             } else {
                 return false;
             }
+        })();
+        window.onload = function () {
+            //var winHeight = typeof window.innerHeight != 'undefined' ? window.innerHeight : document.documentElement.clientHeight;
+            var tip = document.getElementById('weixin-tip-box');
+            if (is_weixin) {
+                tip.style.display = 'block';
+                return false;
+            }
         }
-        var isWeixin = is_weixin();
-        if(isWeixin){
-            $(".weixin-tip").css("height",winHeight);
-            $(".weixin-tip").show();
-        }
-    })
